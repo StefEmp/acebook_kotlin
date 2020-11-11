17c7b4fd5602d4aa78a3cee0b5078eb10e712542
StefEmp <52935365+StefEmp@users.noreply.github.com>
2020-11-11 11:04:07 +0000

    Added spring (template)

---
+++ src/main/kotlin/com/example/landing_page/LandingPageApplication.kt
@@ -0,0 +1,11 @@
+package com.example.landing_page
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LandingPageApplication
+
+fun main(args: Array<String>) {
+	runApplication<LandingPageApplication>(*args)
+}
